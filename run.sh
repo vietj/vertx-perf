f4718b8672a0a3c0b1324ed3247df690d9b1d8a1
Julien Viet <julien@julienviet.com>
2017-04-30 08:30:11 +0200

    Improve scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-java -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0 -XX:+AggressiveOpts -Dio.netty.allocator.numDirectArenas=32 -Dio.netty.allocator.numHeapArenas=32 -Dvertx.disableWebsockets=true -Dvertx.disableH2c=true -Dvertx.disableContextTimings=true -Dvertx.disableTCCL=true -Dvertx.threadChecks=false -Dvertx.host=172.16.0.2 -Dvertx.port=8080 -jar target/server-3.4.2-SNAPSHOT-fat.jar -instances 32
+if [ -z "VERTX_HOST" ]; then
+  VERTX_HOST=172.16.0.2
+fi
+java -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0 -XX:+AggressiveOpts -Dio.netty.allocator.numDirectArenas=32 -Dio.netty.allocator.numHeapArenas=32 -Dvertx.disableWebsockets=true -Dvertx.disableH2c=true -Dvertx.disableContextTimings=true -Dvertx.disableTCCL=true -Dvertx.threadChecks=false -Dvertx.host=$VERTX_HOST -Dvertx.port=8080 -jar target/server-3.4.2-SNAPSHOT-fat.jar -instances 32
